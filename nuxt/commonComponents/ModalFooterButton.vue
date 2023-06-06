995225a761883cc8a9096660295b1bbac7bde0a7
jyodai <tdkdtk5011@gmail.com>
2023-06-06 22:58:11 +0900

    * モーダルのフッターボタンの共通コンポーネントを実装

---
+++ nuxt/commonComponents/ModalFooterButton.vue
@@ -0,0 +1,56 @@
+<template>
+  <div>
+    <v-btn
+      v-for="(button, index) in buttonLists"
+      :key="index"
+      class="modal-footer-button"
+      @click="button.event()"
+    >
+      {{ button.label }}
+    </v-btn>
+  </div>
+</template>
+
+<script>
+
+export default {
+  emit: [
+    'save',
+    'close',
+  ],
+  props: {
+    content: {
+      type    : Object,
+      default : () => {},
+    },
+  },
+  data () {
+    return {
+      buttonLists: [
+        {
+          label : '保存',
+          event : () => this.save(),
+        },
+        {
+          label : '閉じる',
+          event : () => this.close(),
+        },
+      ],
+    }
+  },
+  methods: {
+    save () {
+      this.$emit('save')
+    },
+    close () {
+      this.$emit('close')
+    },
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+.modal-footer-button {
+  margin-right : 10px;
+}
+</style>
