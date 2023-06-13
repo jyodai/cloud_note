537535e564d1423dbced66773c9ba497f6368fec
jyodai <tdkdtk5011@gmail.com>
2023-06-13 21:42:25 +0900

    * axiosのinterceptorsを追加

--- nuxt3/plugins/axios.js
+++ nuxt3/plugins/axios.js
@@ -2,6 +2,10 @@ import axiosCore from "axios";
 
 const axios = axiosCore.create({});
 
+axios.interceptors.response.use((response) => {
+  return response.data
+})
+
 export default defineNuxtPlugin(() => {
   return {
       provide: {
