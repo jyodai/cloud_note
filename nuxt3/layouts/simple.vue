31a9ad162d590f6628a67db4cee5ee5260eb137c
jyodai <tdkdtk5011@gmail.com>
2023-06-13 23:17:48 +0900

    * layoutsディレクトリを移植

---
+++ nuxt3/layouts/simple.vue
@@ -0,0 +1,32 @@
+<template>
+  <v-app dark>
+    <v-app-bar
+      app
+    >
+      <v-toolbar-title v-text="title" />
+      <v-spacer />
+    </v-app-bar>
+    <v-main>
+      <nuxt />
+    </v-main>
+    <v-footer
+      app
+    >
+      <span>&copy; {{ new Date().getFullYear() }}</span>
+    </v-footer>
+  </v-app>
+</template>
+<script>
+
+export default {
+  components: {
+  },
+  data () {
+    return {
+      title: 'Memo',
+    }
+  },
+  mounted () {
+  },
+}
+</script>
