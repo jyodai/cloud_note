70fcffc6a366939cf6223b52f1a49753c305f85f
jyodai <tdkdtk5011@gmail.com>
2023-06-15 20:14:05 +0900

    * ログイン状態を維持する

---
+++ nuxt3/middleware/router.global.js
@@ -0,0 +1,34 @@
+export default defineNuxtRouteMiddleware(async (to) => {
+  const app = useNuxtApp()
+  if (to.name === 'Login') {
+    return
+  }
+
+  if (await tokenCheck(app, app.$store) === false) {
+    return navigateTo('/login')
+  }
+
+  return
+})
+
+async function tokenCheck (app, store) {
+  if (!('sessionStorage' in window) || (window.sessionStorage === null)) {
+    // ストレージ使用不可
+    return false
+  }
+
+  const token = sessionStorage.getItem('token')
+  if (!token) {
+    return false
+  }
+
+  store.commit('User/setToken', token)
+
+  const queryStr = '?token=' + token
+  const response = await app.$axios.get(process.env.API_SERVER_URl + '/user' + queryStr)
+  if (response.user) {
+    store.commit('User/setUser', response.user)
+  }
+
+  return true
+}
