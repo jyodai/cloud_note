e6827d1af6cc52834884e9ba185b00b489aa0cdd
jyodai <tdkdtk5011@gmail.com>
2023-06-17 04:20:35 +0900

    * object用のutilを作成

--- nuxt3/plugins/util.js
+++ nuxt3/plugins/util.js
@@ -1,7 +1,9 @@
-import Load from '~/utils/load.js';
+import load from '~/utils/load.js';
+import object from '~/utils/object.js';
 
 const util = {
-  load : Load,
+  load,
+  object
 };
 
 export default defineNuxtPlugin(() => {
