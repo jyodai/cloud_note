f10d3c2c32e50402462ee852c577aa5ddb1cfc23
jyodai <tdkdtk5011@gmail.com>
2023-06-18 02:46:01 +0900

    * markdonwitのオプションを調整

--- nuxt3/plugins/markdownit.js
+++ nuxt3/plugins/markdownit.js
@@ -1,7 +1,12 @@
 import MarkdownIt from 'markdown-it'
 
 export default defineNuxtPlugin(() => {
-  const instance = new MarkdownIt({})
+  const instance = new MarkdownIt({
+    html       : true, // HTML タグを有効にする
+    breaks     : false, // 改行コードを<br>に変換する
+    linkify    : false, // URLに似たテキストをリンクに自動変換する
+    typography : false, // 言語に依存しないきれいな 置換 + 引用符 を有効にします。
+  })
   return {
       provide: {
         md : instance,
