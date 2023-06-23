0a0b316969d6d4ad388e156d9bb64e2f63c586fb
jyodai <tdkdtk5011@gmail.com>
2023-06-24 01:28:39 +0900

    * utilにsessionStorageを追加

--- nuxt3/plugins/util.js
+++ nuxt3/plugins/util.js
@@ -1,9 +1,11 @@
 import load from '~/utils/load.js';
 import object from '~/utils/object.js';
+import sessionStorage from '~/utils/sessionStorage.js';
 
 const util = {
   load,
-  object
+  object,
+  sessionStorage,
 };
 
 export default defineNuxtPlugin(() => {
