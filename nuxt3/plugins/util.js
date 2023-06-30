4f10674a1318d6404df6e3825ab08d803e47be3b
jyodai <tdkdtk5011@gmail.com>
2023-06-30 16:41:11 +0900

    * 旧ローディング処理を削除

--- nuxt3/plugins/util.js
+++ nuxt3/plugins/util.js
@@ -1,10 +1,8 @@
-import load from '~/utils/load.js';
 import object from '~/utils/object.js';
 import sessionStorage from '~/utils/sessionStorage.js';
 import localStorage from '~/utils/localStorage.js';
 
 const util = {
-  load,
   object,
   sessionStorage,
   localStorage,
