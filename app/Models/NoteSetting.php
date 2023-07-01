73f8dd5575d1e033bb51947c2e2e9b8f93e30a04
jyodai <tdkdtk5011@gmail.com>
2023-07-01 19:28:30 +0900

    * notes_settingsのモデルを作成

---
+++ app/Models/NoteSetting.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class NoteSetting extends Model
+{
+    protected $table = 'notes_settings';
+
+    protected $fillable = [
+        'user_id',
+        'editor_option',
+    ];
+
+    protected $casts = [
+         'user_id' => 'integer',
+    ];
+}
