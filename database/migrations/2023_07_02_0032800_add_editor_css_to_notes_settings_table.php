99030bd1b839abc30c228953d5af4944a690c0e4
jyodai <tdkdtk5011@gmail.com>
2023-07-02 04:05:42 +0900

    * 初期値は空文字にする

--- database/migrations/2023_07_02_0032800_add_editor_css_to_notes_settings_table.php
+++ database/migrations/2023_07_02_0032800_add_editor_css_to_notes_settings_table.php
@@ -3,6 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use App\Models\NoteSetting;
 
 class AddEditorCssToNotesSettingsTable extends Migration
 {
@@ -11,6 +12,7 @@ class AddEditorCssToNotesSettingsTable extends Migration
         Schema::table('notes_settings', function (Blueprint $table) {
             $table->text('editor_css')->after('editor_option');
         });
+        $this->dml();
     }
 
     public function down()
@@ -18,5 +20,14 @@ class AddEditorCssToNotesSettingsTable extends Migration
         Schema::table('notes_settings', function (Blueprint $table) {
             $table->dropColumn('editor_css');
         });
+    }
+
+    public function dml()
+    {
+        $settings = NoteSetting::all();
+        foreach ($settings as $setting) {
+            $setting->editor_css = '';
+            $setting->save();
+        }
     }
 }
