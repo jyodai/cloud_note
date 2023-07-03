4ba7cf062c6618b9d50f2708486dd2ba1c3b5b07
jyodai <tdkdtk5011@gmail.com>
2023-07-03 14:49:49 +0900

    * ユーザ編集のバリデーション

--- app/Http/Requests/User/UpdateRequest.php
+++ app/Http/Requests/User/UpdateRequest.php
@@ -10,9 +10,9 @@ class UpdateRequest extends FormRequest
     {
         $userId = $this->input('id');
         return [
-            'name'     => 'required',
-            'email'    => "required|email|unique:users,email,$userId",
-            'password' => 'required|min:4',
+            'name'     => 'required|string|max:255',
+            'email'    => "required|string|email|unique:users,email,$userId|max:191",
+            'password' => 'required|string|min:4|max:255',
         ];
     }
 
@@ -20,11 +20,17 @@ class UpdateRequest extends FormRequest
     {
         return [
             'name.required'     => 'ユーザー名は必須です。',
+            'name.string'       => 'ユーザ名は文字列で入力してください。',
+            'name.max'          => 'ユーザ名は最大で255文字まで入力できます。',
             'email.required'    => 'メールアドレスは必須です。',
+            'email.string'      => 'メールアドレスは文字列で入力してください。',
             'email.email'       => '有効なメールアドレスの形式で入力してください。',
             'email.unique'      => '入力されたメールアドレスは既に使用されています。',
+            'email.max'         => 'メールアドレスは最大で255文字まで入力できます。',
             'password.required' => 'パスワードは必須です。',
+            'password.string'   => 'パスワードは文字列で入力してください。',
             'password.min'      => 'パスワードは少なくとも8文字以上で入力してください。',
+            'password.max'      => 'パスワードは最大で255文字まで入力できます。',
         ];
     }
 }
