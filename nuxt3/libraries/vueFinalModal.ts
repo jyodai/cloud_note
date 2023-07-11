946b09a970cee3351bda75a6924d0e26848f1f58
jyodai <tdkdtk5011@gmail.com>
2023-07-11 22:10:47 +0900

    * tsに変更

---
+++ nuxt3/libraries/vueFinalModal.ts
@@ -0,0 +1,92 @@
+import { Vfm, Modal} from 'vue-final-modal';
+
+interface ExtendedModal extends Modal {
+  params?: object;
+  closedCallback?: () => void;
+}
+
+export default class VueFinalModal {
+  private vfm: Vfm;
+  private closedInfo: { id: string | null; closeType: number | null } = {
+    id        : null,
+    closeType : null,
+  };
+
+  constructor(vfm: Vfm) {
+    this.vfm = vfm;
+  }
+
+  public get(id: string): ExtendedModal | undefined {
+    return this.vfm.get(id) as ExtendedModal | undefined;
+  }
+
+  public open(id: string, params: object | null = null): void {
+    this.vfm.open(id);
+    if (params !== null) {
+      this.setParams(id, params);
+    }
+  }
+
+  public close(id: string, closeType: number): void {
+    this.vfm.close(id);
+    this.unsetParams(id);
+    this.setClosedInfo(id, closeType);
+    this.execClosedCallback(id);
+    this.unsetClosedCallback(id);
+  }
+
+  public setClosedCallback(id: string, callback: () => void): void {
+    const modal = this.get(id);
+    if (modal) {
+      modal.closedCallback = callback;
+    }
+  }
+
+  public getClosedInfo(): object {
+    return this.closedInfo;
+  }
+
+  private setClosedInfo(id: string, closeType: number): void {
+    this.closedInfo = {
+      id,
+      closeType,
+    };
+  }
+
+  private unsetClosedCallback(id: string): void {
+    const modal = this.get(id);
+    if (modal) {
+      delete modal.closedCallback;
+    }
+  }
+
+  private execClosedCallback(id: string): void {
+    const modal = this.get(id);
+    if (modal && modal.closedCallback) {
+      modal.closedCallback();
+    }
+  }
+
+  public setParams(id: string, params: object): void {
+    const modal = this.get(id);
+    if (modal) {
+      modal.params = params;
+    }
+  }
+
+  public getParams(id: string): object {
+    const modal = this.get(id);
+    if (modal && modal.params) {
+      return modal.params;
+    }
+    return {};
+  }
+
+  private unsetParams(id: string): void {
+    const modal = this.get(id);
+    if (modal && modal.params) {
+      delete modal.params;
+    }
+  }
+}
+
