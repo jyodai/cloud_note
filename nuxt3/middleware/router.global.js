82155d31497faa9808a2f803d2f48b14f7bbfa8a
jyodai <tdkdtk5011@gmail.com>
2023-07-14 10:30:32 +0900

    * トークンが変更していた場合の対応

--- nuxt3/middleware/router.global.js
+++ nuxt3/middleware/router.global.js
@@ -8,7 +8,8 @@ export default defineNuxtRouteMiddleware(async (to) => {
     return navigateTo('/login');
   }
 
-  const user = await app.$axios.get(app.$config.public.apiUrl + '/users/me');
+  const user = await app.$axios.get(app.$config.public.apiUrl + '/users/me')
+    .catch(() => { return; });
   if (!user) {
     return navigateTo('/login');
   }
