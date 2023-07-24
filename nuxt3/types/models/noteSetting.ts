6bf157b3b4e6222e574941645389ed0106168fc0
jyodai <tdkdtk5011@gmail.com>
2023-07-24 16:05:32 +0900

    * 不要ファイルの削除

--- nuxt3/types/models/noteSetting.ts
+++
@@ -1,8 +0,0 @@
-export default interface NoteSetting {
-  id: number;
-  user_id: number;
-  editor_option: string;
-  editor_css: string;
-  created_at: string;
-  updated_at: string;
-}
--- nuxt3/types/models/user.ts
+++
@@ -1,9 +0,0 @@
-import NoteSetting from '~/types/models/noteSetting';
-
-export default interface User {
-    id: number;
-    name: string;
-    user_type: number;
-    email: string;
-    note_setting: NoteSetting;
-}
