d0d689409581d7350565f90ba30b3ca0f664012a
jyodai <tdkdtk5011@gmail.com>
2023-08-02 03:26:48 +0900

    * フラグの型はboolean

--- app/Consts/Common.php
+++ app/Consts/Common.php
@@ -4,8 +4,8 @@ namespace App\Consts;
 
 class Common
 {
-    public const FLAG_ON  = 1;
-    public const FLAG_OFF = 0;
+    public const FLAG_ON  = true;
+    public const FLAG_OFF = false;
 
     public const INCREMENT = 10;
 }
