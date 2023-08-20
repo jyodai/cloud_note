1aaebcee758c374f47b06217702b806e56bcd561
jyodai <tdkdtk5011@gmail.com>
2023-08-20 15:52:13 +0900

    * テキストカラーに変数を使用する

--- nuxt3/components/Note/NoteContentHead.vue
+++ nuxt3/components/Note/NoteContentHead.vue
@@ -49,9 +49,9 @@ export default {
 .title-area {
   width: 100%;
   text-align: left;
-  opacity: 0.6;
   font-size: 14px;
   white-space: nowrap;
   overflow: auto;
+  color: $color-text-primary-dark;
 }
 </style>
